17bda65a09860128ab45279f15569bf7b7714327
Ibrohim-06 <ibrohim.sadriddinov06@gmail.com>
2025-09-19 19:42:43 +0500

    fix

--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,16 @@
 import React from 'react'
 import { hydrateRoot } from 'react-dom/client'
 import App from './App.jsx'
-import { BrowserRouter } from 'react-router-dom'
+import { HashRouter } from 'react-router-dom'
 import { AppProvider } from './context/context.jsx'
 
 const container = document.getElementById('root')
 
 hydrateRoot(
   container,
-  <BrowserRouter>
+  <HashRouter>
     <AppProvider>
       <App />
     </AppProvider>
-  </BrowserRouter>
+  </HashRouter>
 )
